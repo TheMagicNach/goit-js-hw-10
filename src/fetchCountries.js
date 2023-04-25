2cb0784cf2fbdf686e9dd75e7fac1303a3b44f0e
TheMagicNach <Mrvlados951357@gmail.com>
2023-04-26 00:18:57 +0300

    finish mb

---
+++ src/fetchCountries.js
@@ -0,0 +1,13 @@
+export function fetchCountries(country) {
+  const options = new URLSearchParams({ fields:'name,capital,population,flags,languages'});
+  const url = `https://restcountries.com/v3.1/name/${country}?${options}`;
+
+    return fetch(url)
+      .then(response => {
+        console.log(response)
+        if (!response.ok) {
+          throw new Error()
+        }
+        return response.json();
+  })  
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,73 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries.js';
+import { Notify } from 'notiflix';
+import debounce from 'lodash.debounce';
+
 
 const DEBOUNCE_DELAY = 300;
+const countyList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+const searchBox = document.querySelector('input#search-box');
+
+searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY))
+
+function onSearch(e) {
+  const searchQuery = e.target.value.trim();
+
+  if (!searchQuery) {
+    clearFields();
+    return
+  }
+
+  fetchCountries(searchQuery)
+    .then(
+      data => {
+      if (data.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+        return;
+      }
+      checkСountries(data)
+    })
+    .catch(() => Notify.failure('Oops, there is no country with that name'));
+}
+
+
+function templateCountrieslist(countries) {
+  return countries
+    .map(({ name, flags }) =>
+      `<li class="country_list">  
+        <img src='${flags.png}' alt="${name.official}" width ='60' height='50'>
+        <p class='county-name'>${name.official}</p>
+      </li>`)
+    .join('');
+};
+
+function templateCountryInfo(country) {
+  return country.map(
+    ({ name, flags ,capital, population,  languages }) =>
+      ` <div class="wrapper">
+          <img src="${flags.png}" alt="${name.official}" width="150" height="100">
+          <h1 class="name-country">${name.common}</h1>
+        </div>
+        <ul class="info-list">
+          <li class="info-item">Capital: ${capital}</li>
+          <li class="info-item">Population: ${population}</li>
+          <li class="info-item">Languages: ${Object.values(languages)}</li>
+        </ul>`
+  );
+}
+
+function clearFields(){
+  countryInfo.innerHTML = '';
+  countyList.innerHTML = '';
+}
+
+function checkСountries(country){
+  if (country.length === 1) {
+    countyList.innerHTML = "";
+    countryInfo.innerHTML = templateCountryInfo(country);
+  } else {  
+    countryInfo.innerHTML = "";
+    countyList.innerHTML = templateCountrieslist(country);
+  }
+}
